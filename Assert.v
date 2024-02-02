3535797fddc2b8804a38e130bb27e7f9a6531f74
nrethans <nick.rethans@gmail.com>
2024-02-01 22:50:58 -0600

    Assert

--- Assert.v
+++ Assert.v
@@ -1,5 +1,6 @@
-`define assert(actual, expected) \
-    $display("Time: "); write($realtime); \
+`define assert(actual, expected, description) \
+    if(description) $display(description); \
+    $write("Time: "); $display($realtime); \
     if(actual == expected) \
         $display("-- Test Passed --"); \
     else \
