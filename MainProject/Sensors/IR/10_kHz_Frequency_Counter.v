f9c1ac5b45d5a571d3eb8dffc05505072ee798b5
nrethans <145207516+nrethans@users.noreply.github.com>
2024-04-04 13:37:00 -0500

    FreqCounters

--- MainProject/Sensors/IR/10_kHz_Frequency_Counter.v
+++ MainProject/Sensors/IR/10_kHz_Frequency_Counter.v
@@ -22,50 +22,36 @@ module FrequencyCounter(
     input ir,       // Input signal
     input enable,    // Enable signal
     input reset,     // Reset signal
-    output reg [15:0] Hz,   // Output frequency
-    output reg ir1k,        // 1kHz indicator
-    output reg ir10k        // 10kHz indicator
+    output reg ir10k = 1'b0       // 10kHz indicator
 );
 
-    reg [1:0] EdgeTest = 0;
-    reg [15:0] temp = 0;
-    reg [23:0] count = 0;
+    reg [1:0] EdgeTest = 2'b0;
+    reg [15:0] Hz = 16'b0, temp = 16'b0;
+    reg [23:0] count = 24'b0;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            Hz <= 0;
-            ir1k <= 0;
-            ir10k <= 0;
-            temp <= 0;
-            count <= 0;
+            Hz <= 16'b0;
+            ir10k <= 1'b0;
+            temp <= 16'b0;
+            count <= 24'b0;
         end else if (enable) begin
             EdgeTest[0] <= ir;
             if (!EdgeTest[0] && EdgeTest[1]) begin
                 temp <= temp + 1;
             end
             EdgeTest[1] <= EdgeTest[0];
-            
             // Update Hz every 10 million clock cycles (for 100 MHz clock)
             if (count == 10_000_000) begin
                 Hz <= temp * 10; // Scale to fit 100 MHz
-                
-                if(Hz == 1000) begin
-                    ir1k <= 1;
-                end
-                else if (Hz == 10000) begin
-                    ir10k <= 1;
-                end
-                else begin
-                    ir1k <= 0;
-                    ir10k <= 0;
-                end
-                
+                if (Hz >= 9000 && Hz <=11000) ir10k <= 1;
+                else ir10k <= 0;
                 temp <= 0;
                 count <= 0;
-            end else begin
-                count <= count + 1;
-            end
-        end else begin
+            end 
+            else count <= count + 1;
+        end 
+        else begin
             // Reset internal state if enable is low
             temp <= 0;
             count <= 0;
---
+++ MainProject/Sensors/IR/1_kHz_Frequency_Counter.v
@@ -0,0 +1,59 @@
+`timescale 1ns/1ns
+/*
+    ECE 3331-303 Group 3
+    Spring 2024
+    
+    Name: Samir Hossain
+    Module Name: FrequencyCounter
+    Submodule of:
+    Dependances:
+    Description: Using ir signal calculates frequency
+
+    Inputs: clk, ir, enable, reset
+
+    Outputs: [16:0] Hz, ir1k, ir10k
+
+    Notes: 
+
+*/
+
+module FrequencyCounter(
+    input clk,       // Clock input
+    input ir,       // Input signal
+    input enable,    // Enable signal
+    input reset,     // Reset signal
+    output reg ir1k = 1'b0     // 1kHz indicator
+);
+
+    reg [1:0] EdgeTest = 2'b0;
+    reg [15:0] Hz = 16'b0, temp = 16'b0;
+    reg [23:0] count = 24'b0;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            Hz <= 16'b0;
+            ir1k <= 1'b0;
+            temp <= 16'b0;
+            count <= 24'b0;
+        end 
+        else if (enable) begin
+            EdgeTest[0] <= ir;
+            if (!EdgeTest[0] && EdgeTest[1]) temp <= temp + 1;
+            EdgeTest[1] <= EdgeTest[0];
+            // Update Hz every 10 million clock cycles (for 100 MHz clock)
+            if (count == 10_000_000) begin
+                Hz <= temp * 10; // Scale to fit 100 MHz
+                if(Hz >= 900 && Hz =<1200) ir1k <= 1;
+                else ir1k <= 0;    
+                temp <= 0;
+                count <= 0;
+            end 
+            else count <= count + 1;
+        end 
+        else begin
+            // Reset internal state if enable is low
+            temp <= 0;
+            count <= 0;
+        end
+    end
+endmodule
