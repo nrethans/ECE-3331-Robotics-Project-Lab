4364121b43ca9d7e5c4b5806c3661d000f297d2c
nrethans <145207516+nrethans@users.noreply.github.com>
2024-05-30 23:24:37 -0500

    final

--- MainProject/DriveTrain/MotorPWM/MotorPWM.v
+++ MainProject/DriveTrain/MotorPWM/MotorPWM.v
@@ -17,9 +17,9 @@
 */
 module MotorPWM(input clk, input [1:0] DutyCycle, output reg PulseSignal=1'b0);
     reg [13:0] count = 14'b0000000000000;
-    parameter QuarterDuty = 14'd5000, //5,000
-                 HalfDuty = 14'b01100001101010, //6250
-         ThreeQuarterDuty = 14'd10000; //10,000
+    parameter FortyDuty = 14'd5000, //5,000
+               HalfDuty = 14'b01100001101010, //6250
+             EightyDuty = 14'd10000; //10,000
     always @(posedge clk) begin
         if(count >= 14'b11000011010100)begin                                    
             count = 14'b00000000000000;
@@ -28,9 +28,9 @@ module MotorPWM(input clk, input [1:0] DutyCycle, output reg PulseSignal=1'b0);
         else begin
             count = count + 1'b1;
         end 
-        case(DutyCycle) //00 = 25% duty, 01 = 50% duty, 10 = 75% duty, 11 = 100%   
+        case(DutyCycle) //00 = 40% duty, 01 = 50% duty, 10 = 75% duty, 11 = 100%   
             2'b00: begin
-                if(count<=QuarterDuty) PulseSignal = 1'b1;
+                if(count<=FortyDuty) PulseSignal = 1'b1;
                 else PulseSignal = 1'b0;
             end
             2'b01: begin
@@ -38,7 +38,7 @@ module MotorPWM(input clk, input [1:0] DutyCycle, output reg PulseSignal=1'b0);
                 else PulseSignal = 1'b0;
             end
             2'b10: begin
-                if(count<=ThreeQuarterDuty) PulseSignal = 1'b1;
+                if(count<=EightyDuty) PulseSignal = 1'b1;
                 else PulseSignal = 1'b0;
             end
             2'b11: PulseSignal = 1'b1;
@@ -46,3 +46,4 @@ module MotorPWM(input clk, input [1:0] DutyCycle, output reg PulseSignal=1'b0);
         endcase
     end
 endmodule 
+
